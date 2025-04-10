e12c277ef0256515f7dd71b77555b61e259b0010
Kelly Tran <kellytran03@berkeley.edu>
2025-04-10 14:01:38 -0700

    reroute main landing page to login for now

--- tastebud-app/src/AppRoutes.jsx
+++ tastebud-app/src/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '@/pages/Login';
 import Signup from '@/pages/Signup';
 import Home from '@/pages/Home';
@@ -10,16 +10,19 @@ import PrivateRoute from '@/components/PrivateRoute';
 const AppRoutes = () => {
   return (
     <Routes>
-      {/* public default routes */}
+      {/* default root route redirects to login */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+      {/* public routes */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
 
-      {/* protected routes - only for logged in users */}
+      {/* protected routes */}
       <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
       <Route path="/recipes" element={<PrivateRoute><Recipes /></PrivateRoute>} />
       <Route path="/profile" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
 
-      {/* 404 page */}
+      {/* catch-all for 404s */}
       <Route path="*" element={<h2>404 Error - Page Not Found</h2>} />
     </Routes>
   );
