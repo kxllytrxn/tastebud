759bfbbf9588b905db70bd0e2776ca1f3b626e2b
eattie <fillet@gmail.com>
2025-05-01 16:19:59 -0700

    right sidebar redirects working

--- tastebud-app/src/components/ManageAccount/ManageAccount.jsx
+++ tastebud-app/src/components/ManageAccount/ManageAccount.jsx
@@ -5,8 +5,25 @@ import mycircle from '@/assets/icons/mycircle-black-fill.png'
 import savedrecipes from '@/assets/icons/save-black-fill.png'
 import utensils from '@/assets/icons/utensils-black-fill.png'
 import signout from '@/assets/icons/sign-out.png'
+import { logoutUser } from '@/utils/auth';
+import { useNavigate } from 'react-router-dom';
 
 const ManageAccount = () => {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    logoutUser();
+    navigate('/login');
+  };
+
+  const handleSavedRecipesClick = () => {
+    navigate('/recipes');
+  };
+  
+  const handleMyRecipesClick = () => {
+    navigate('/profile');
+  };
+
   return (
     <div className="manage-account-card">
       <div className="header-bar"></div>
@@ -18,17 +35,17 @@ const ManageAccount = () => {
           <span>My circle</span>
         </li>
 
-        <li className="menu-entry"> 
+        <li className="menu-entry" onClick={handleSavedRecipesClick}> 
           <img src={savedrecipes} alt="Saved" className="menu-icon" />
           <span>Saved recipes</span>
         </li>
 
-        <li className="menu-entry"> 
+        <li className="menu-entry" onClick={handleMyRecipesClick}> 
           <img src={utensils} alt="Recipe Icon" className="menu-icon" />
           <span>My recipes</span>
         </li>
 
-        <li className="menu-entry"> 
+        <li className="menu-entry" onClick={handleSignOut}> 
           <img src={signout} alt="Sign Out" className="menu-icon" />
           <span>Sign out</span>
         </li>
